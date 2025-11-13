d70626429ab8b46e40251d3a96000527f40a0cc9
25LP6386 25LP6386 <tsatskanaraa@gmail.com>
2025-11-13 12:21:24 +0800

    some

---
+++ index.js
@@ -0,0 +1,21 @@
+// 1. npm init -y
+// It creates package.json
+// 2. npm i express cors
+// It creates node_modules and package-lock.jsons
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 999;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Yo what's up my dudes   ");
+});
+
+app.listen(PORT, () => {
+  console.log(`API listening on http://localhost:${PORT}`);
+});
